e75335deb2528a26fafaa15e6213de74399cbacf
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-03-24 08:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+   let arr1 = [];
+	let targ = 0;
+	for(let i=0;i<arr.length;i++){
+		
+		arr1.push(arr[i]);
+		
+		arr1 += targ  +1;
+	}
+	return arr1;
 }
 
 module.exports = threeSum;
