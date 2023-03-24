e5b304fddf4a3d153f0c73bc82368d7491955105
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-03-24 23:56:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 function threeSum(arr, target) {
-// write your code here
- //   let arr1 = [];
-	// let targ = 0;
-	// for(let i=0;i<arr.length;i++){
-		
-	// 	arr1.push(arr[i]);
-		
-	// 	arr1 += targ+arr[i];
-	// }
-	// return arr1;
-	arr.sort((a,b)=>a-b);
-let ans =9999999;
-	const n=arr.length;
-	for(let i=0;i<n-2;i++){
-		for(let j=i+1;j<n-1;j++){
-			for(let k=j+1;k<n;k++){
-				let sum=arr[i]+arr[j]+arr[k];
-				if(Math.abs(target-sum)<ans){
-					ans = sum;
-				}
-			}
-		}
-	}
-	return ans
+  //your code here
+  arr.sort((a,b)=>a-b)
+ 
+        // console.log(arr)
+        let len = arr.length
+        let closestSum = 10000000;
+        for(let i=0;i<=len-3;i++){
+            // len-1
+            // len-2
+            // len-3
+            let left = i+1;
+            let right = len-1;
+ 
+            while(left<right){
+                let currSum = arr[i]+arr[left]+arr[right]
+ 
+                if(Math.abs(currSum-target)<Math.abs(closestSum-target))
+                    closestSum = currSum
+ 
+                if(currSum>target)
+                    right--
+                else
+                    left++
+            }
+        }
+        return closestSum;
 }
-
+ 
 module.exports = threeSum;
